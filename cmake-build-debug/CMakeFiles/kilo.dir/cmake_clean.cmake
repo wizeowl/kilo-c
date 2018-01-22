2dc0b6df92dd62564151c6d040fa8b0d17db53ec
wizeowl <nidhal.ben.tahar@gmail.com>
2018-01-22 16:18:02 +0100

    disabling ctrl-q, ctrl-s

---
+++ cmake-build-debug/CMakeFiles/kilo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kilo.dir/kilo.c.o"
+  "kilo.pdb"
+  "kilo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kilo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- kilo.c
+++ kilo.c
@@ -7,30 +7,38 @@
 struct termios orig_termios;
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    struct termios raw = orig_termios;
+    // c_lflag: local flags
+    // disable ECHO
+    // disable ICANON (so we can read char by char instead of wait for enter)
+    // disable ISIG for ctrl-c and ctrl-v
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    // disable ctrl-s(suspend output) and ctrl-q(resume output)
+    raw.c_lflag &= ~(IXON);
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
-  enableRawMode();
+    enableRawMode();
 
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-    if (iscntrl(c)) {
-      printf("%d\n", c);
-    } else {
-      printf("%d ('%c')\n", c, c);
+    char c;
+    // read one char for standard input into c
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)/*form <ctype.h>, is this a control character?*/) {
+            printf/*from <stdio.h>*/("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
