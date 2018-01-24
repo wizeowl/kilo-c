3c741602082bae5f877039b06f186adea1d67f4c
wizeowl <nidhal.ben.tahar@gmail.com>
2018-01-24 16:26:19 +0100

    displaying cursor

--- kilo.c
+++ kilo.c
@@ -17,6 +17,7 @@
 /*** data ***/
 
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -179,8 +180,11 @@ void editorRefreshScreen() {
 
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
-    abAppend(&ab, "\x1b[?25l", 6);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+    
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
@@ -189,6 +193,9 @@ void editorRefreshScreen() {
 /*** init ***/
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
