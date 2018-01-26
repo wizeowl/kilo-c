2db96ceda9fd82211a47ae49bdedfab827a0fe72
wizeowl <nidhal.ben.tahar@gmail.com>
2018-01-26 18:41:46 +0100

    supporting delete key

--- kilo.c
+++ kilo.c
@@ -19,6 +19,7 @@ enum editorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    DEL_KEY,
     HOME_KEY,
     END_KEY,
     PAGE_UP,
@@ -97,6 +98,8 @@ int editorReadKey() {
                     switch (seq[1]) {
                         case '1':
                             return HOME_KEY;
+                        case '3':
+                            return DEL_KEY;
                         case '4':
                             return END_KEY;
                         case '5':
